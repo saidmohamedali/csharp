f7f9e3e660800378dafc7b773ec393f53ca91922
MOHAMED ALI Saïd <saidkove@yahoo.fr>
2014-08-02 22:53:06 +0200

    Adding Scenario 3 code

---
+++ CharpsMaintTopics/Generics/Constraints/Scenario3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        class A { public A() { } }
+
+        struct C {  }
+
+        class D<T> where T : new()
+        {
+            D<A> c1 = new D<A>();  
+            D<C> c3 = new D<C>();  
+        }
+    }
+}
