fef2af4d85c8168675e1c976d4131179bafda2ad
MOHAMED ALI Saïd <saidkove@yahoo.fr>
2014-08-05 14:21:29 +0200

    updating  Variance Scenario2

--- CharpsMaintTopics/Generics/Variance/Scenario2/Program.cs
+++ CharpsMaintTopics/Generics/Variance/Scenario2/Program.cs
@@ -37,10 +37,19 @@ namespace Scenario2
             l_instrument = l_indexOption;
             Console.WriteLine(l_instrument);
 
-            Instrument[] array = new IndexOption[10];
-            array[0] = new Index();
-            array[1] = new Instrument();
-            Console.WriteLine(array);
+            Instrument[] l_instruments = new Instrument[3];
+            l_instruments[0] = new Index();
+            l_instruments[1] = new Instrument();
+            l_instruments[2] = new IndexOption();
+            for (int l_i = 0; l_i < l_instruments.Length; ++l_i)
+                Console.WriteLine(string.Format("array[{0}]={1}:", l_i, l_instruments[l_i]));
+
+            Instrument[] l_instruments2 = new IndexOption[3];
+            l_instruments2[0] = new Index(); //Runtime Exception
+            l_instruments2[1] = new Instrument();//Runtime Exception
+            l_instruments2[2] = new IndexOption();
+            for (int l_i = 0; l_i < l_instruments2.Length; ++l_i)
+                Console.WriteLine(string.Format("array[{0}]={1}:", l_i, l_instruments2[l_i]));
             Console.Read();
         }
 
