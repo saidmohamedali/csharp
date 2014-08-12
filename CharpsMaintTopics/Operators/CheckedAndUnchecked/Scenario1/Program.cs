bddc495b2bc2111f01d3ae0a37a6c17b10d51b12
MOHAMED ALI Saïd <saidkove@yahoo.fr>
2014-08-12 13:52:47 +0200

    Cheched And Unchecked scenario

---
+++ CharpsMaintTopics/Operators/CheckedAndUnchecked/Scenario1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ten = 10;
+            int i2 = 2147483647 + ten;
+           
+
+            Console.WriteLine(i2 == 2147483657);
+
+            // Checked expression.
+            Console.WriteLine(checked(2147483647 + ten));
+
+            // Checked block. 
+            checked
+            {
+                int i3 = 2147483647 + ten;
+                Console.WriteLine(i3);
+            }
+            Console.Read();
+        }
+    }
+}
