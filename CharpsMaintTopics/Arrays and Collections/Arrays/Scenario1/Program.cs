55c0ed0035ebeb63db9b1f68ad3c0a3318b77201
MOHAMED ALI Saïd <saidkove@yahoo.fr>
2014-08-13 15:17:24 +0200

    Adding Scenarios for Arrays

---
+++ CharpsMaintTopics/Arrays and Collections/Arrays/Scenario1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario1
+{
+    class Program
+    {
+       
+        static void Main()
+        {
+           
+            object[] objectArray = new int[] { 1, 2, 4 };
+            
+            System.Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ CharpsMaintTopics/Arrays and Collections/Arrays/Scenario2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] objectArray = new int[] { 1, 2, 4 };
+            Array myArray = (Array)objectArray;
+            
+         
+            System.Console.WriteLine(myArray.GetType() == typeof(Array));
+            Console.WriteLine(myArray.GetType().BaseType == typeof(Array));
+            System.Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
